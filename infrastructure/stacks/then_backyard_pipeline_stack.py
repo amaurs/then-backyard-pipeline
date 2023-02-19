8182f784229f71017e3c80db187af57cec9bbe6e
amaurs <amaury.gtz@gmail.com>
2023-02-19 13:50:37 -0800

    🚀 Preparing for initial deployment.

--- infrastructure/stacks/then_backyard_pipeline_stack.py
+++ infrastructure/stacks/then_backyard_pipeline_stack.py
@@ -1,38 +1,20 @@
 import os
 
 import aws_cdk as cdk
-from aws_cdk import pipelines
+from aws_cdk.pipelines import CodePipeline, ShellStep, CodePipelineSource
 from constructs import Construct
 
 
 class ThenBackyardPipelineStack(cdk.Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
-        super().__init__(scope, id, **kwargs)
+        super().__init__(scope, construct_id, **kwargs)
 
-        pipeline = pipelines.CodePipeline(
-            scope=scope,
-            id="Pipeline",
-            pipeline_name="ThenPipelineBackyard",
-            synth=pipelines.ShellStep(
-                'Build',
-                input=pipelines.CodePipelineSource.git_hub(
-                    repo_string='amaurs/then-backyard-pipeline',
-                    branch='main',
-                    authentication=cdk.SecretValue.secrets_manager(
-                        os.getenv('GITHUB_PERSONAL_ACCESS_TOKEN_SECRET_NAME'))),
-                additional_inputs={
-                    "multi-arm-bandits": pipelines.CodePipelineSource.git_hub(
-                        repo_string='amaurs/then-backyard',
-                        branch='master',
-                        authentication=cdk.SecretValue.secrets_manager(
-                            os.getenv('GITHUB_PERSONAL_ACCESS_TOKEN_SECRET_NAME')))},
-                env={},
-                primary_output_directory='cdk.out',
-                commands=[
-                    'ls',
-                    "npm install -g aws-cdk",
-                    'python -m pip install -r requirements.txt',
-                    'cdk synth',
-                ]
-            )
-        )
+        pipeline = CodePipeline(self, "Pipeline",
+                        pipeline_name="ThenBackyardPipeline",
+                        synth=ShellStep("Synth",
+                            input=CodePipelineSource.git_hub("amaurs/then-backyard-pipeline", "main"),
+                            commands=["npm install -g aws-cdk",
+                                "python -m pip install -r requirements.txt",
+                                "cdk synth"]
+                        )
+                    )
