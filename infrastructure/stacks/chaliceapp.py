9dc1a908fac9078f7a7b1e431c4528f2a6d70102
amaurs <amaury.gtz@gmail.com>
2023-02-22 00:44:21 -0800

    🤞🏽 Reverting to EDGE and creating new certificate.

--- infrastructure/stacks/chaliceapp.py
+++ infrastructure/stacks/chaliceapp.py
@@ -1,6 +1,7 @@
 import os
 
 import aws_cdk as cdk
+from aws_cdk.aws_certificatemanager import Certificate, CertificateValidation
 from aws_cdk.aws_route53 import HostedZone, CfnRecordSet
 
 from chalice.cdk import Chalice
@@ -15,34 +16,43 @@ class ChaliceApp(cdk.Stack):
     def __init__(self, scope, id, **kwargs):
         super().__init__(scope, id, **kwargs)
 
-        domain_name = os.environ["API_GATEWAY_CUSTOM_DOMAIN"]
-
-        self.chalice = Chalice(
-            self, 'ChaliceApp', source_dir=RUNTIME_SOURCE_DIR,
-            stage_config={
-                "api_gateway_endpoint_type": "REGIONAL",
-                "api_gateway_custom_domain": {
-                    "domain_name": domain_name,
-                    "certificate_arn": os.getenv("CERTIFICATE_ARN"),
-                }
-            }
-        )
-
         # The following approach is based off https://github.com/aws/chalice/issues/1728
 
-        hosted_zone = HostedZone.from_hosted_zone_id(self, "ThenHostedZone", os.getenv("HOSTED_ZONE_ID"))
+        domain_name = "api.then.gallery"
 
-        custom_domain = self.chalice.get_resource("ApiGatewayCustomDomain")
+        self.hosted_zone = HostedZone(
+            self,
+            "ThenHostedZone",
+            zone_name=os.getenv("HOSTED_ZONE_ID"))
+
+        self.acm_cert = Certificate(
+            self,
+            "ThenBackyardCertificate",
+            domain_name=self.domain,
+            validation=CertificateValidation.from_dns(self.hosted_zone))
+
+        self.chalice = Chalice(
+            self, 'ChaliceApp',
+            source_dir=RUNTIME_SOURCE_DIR,
+            stage_config={
+                "api_gateway_endpoint_type": "EDGE",
+                "api_gateway_custom_domain": {
+                    "domain_name": domain_name,
+                    "certificate_arn": self.acm_cert.certificate_arn,
+                }
+            })
+
+        self.custom_domain = self.chalice.get_resource("ApiGatewayCustomDomain")
         CfnRecordSet(
             self,
             "ThenBackyardAPISubdomain",
-            hosted_zone_id=hosted_zone.hosted_zone_id,
+            hosted_zone_id=self.hosted_zone.hosted_zone_id,
             name=domain_name,
             type="A",
             alias_target=CfnRecordSet.AliasTargetProperty(
-                dns_name=custom_domain.get_att("RegionalDomainName").to_string(),
-                hosted_zone_id=custom_domain.get_att(
-                    "RegionalHostedZoneId"
+                dns_name=self.custom_domain.get_att("DistributionDomainName").to_string(),
+                hosted_zone_id=self.custom_domain.get_att(
+                    "DistributionHostedZoneId"
                 ).to_string(),
                 evaluate_target_health=False,
             ),
