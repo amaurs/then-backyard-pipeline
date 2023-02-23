7233f20e7c19ef87041998aedac07ac4470501b3
amaurs <amaury.gtz@gmail.com>
2023-02-22 22:00:44 -0800

    🍀 Adding a zone delegation record to fix not resolvable host.

--- infrastructure/stacks/chaliceapp.py
+++ infrastructure/stacks/chaliceapp.py
@@ -2,7 +2,7 @@ import os
 
 import aws_cdk as cdk
 from aws_cdk.aws_certificatemanager import Certificate, CertificateValidation
-from aws_cdk.aws_route53 import HostedZone, CfnRecordSet
+from aws_cdk.aws_route53 import HostedZone, CfnRecordSet, ZoneDelegationRecord
 
 from chalice.cdk import Chalice
 
@@ -16,42 +16,59 @@ class ChaliceApp(cdk.Stack):
     def __init__(self, scope, id, **kwargs):
         super().__init__(scope, id, **kwargs)
 
-        # The following approach is based off https://github.com/aws/chalice/issues/1728
+        # Deployment of chalice custom domain on cdk is based on https://github.com/aws/chalice/issues/1728
 
-        self.domain_name = "api.then.gallery"
+        aphex_domain = "then.gallery"
+        domain_name = f"if.{aphex_domain}"
 
-        self.hosted_zone = HostedZone(
+        hosted_zone = HostedZone(
             self,
-            "ThenHostedZone",
-            zone_name=self.domain_name)
+            "ThenBackyardHostedZone",
+            zone_name=domain_name,
 
-        self.acm_cert = Certificate(
+        )
+
+        # Using a zone delegation record was taken from https://bahr.dev/2020/09/01/multiple-frontends/
+
+        ZoneDelegationRecord(
+            self,
+            "ThenBackyardZoneDelegationRecord",
+            record_name=domain_name,
+            name_servers=hosted_zone.hosted_zone_name_servers,
+            zone=HostedZone.from_lookup(self, "ThenHostedZone", domain_name=aphex_domain),
+            ttl=cdk.Duration.minutes(1)  # TODO: should increase this value once everything works
+        )
+
+        acm_cert = Certificate(
             self,
             "ThenBackyardCertificate",
-            domain_name=self.domain_name,
-            validation=CertificateValidation.from_dns(self.hosted_zone))
+            domain_name=domain_name,
+            validation=CertificateValidation.from_dns(hosted_zone)
+        )
 
-        self.chalice = Chalice(
-            self, 'ChaliceApp',
+        chalice = Chalice(
+            self,
+            'ChaliceApp',
             source_dir=RUNTIME_SOURCE_DIR,
             stage_config={
                 "api_gateway_endpoint_type": "EDGE",
                 "api_gateway_custom_domain": {
-                    "domain_name": self.domain_name,
-                    "certificate_arn": self.acm_cert.certificate_arn,
+                    "domain_name": domain_name,
+                    "certificate_arn": acm_cert.certificate_arn,
                 }
-            })
+            }
+        )
 
-        self.custom_domain = self.chalice.get_resource("ApiGatewayCustomDomain")
+        custom_domain = chalice.get_resource("ApiGatewayCustomDomain")
         CfnRecordSet(
             self,
             "ThenBackyardAPISubdomain",
-            hosted_zone_id=self.hosted_zone.hosted_zone_id,
-            name=self.domain_name,
+            hosted_zone_id=hosted_zone.hosted_zone_id,
+            name=domain_name,
             type="A",
             alias_target=CfnRecordSet.AliasTargetProperty(
-                dns_name=self.custom_domain.get_att("DistributionDomainName").to_string(),
-                hosted_zone_id=self.custom_domain.get_att(
+                dns_name=custom_domain.get_att("DistributionDomainName").to_string(),
+                hosted_zone_id=custom_domain.get_att(
                     "DistributionHostedZoneId"
                 ).to_string(),
                 evaluate_target_health=False,
